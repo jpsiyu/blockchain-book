5024714930b5f634fc96f3e40ac8f7d1a19e13ce
jpsiyu <jpsiyu@163.com>
2018-12-01 11:05:43 +0800

    css

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62833" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49389" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
